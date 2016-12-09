424f0a4a3737f92aaf932ffbc3cb3e4da345ef42
高宇 <gaoyu19921116@foxmail.com>
2016-12-09 14:08:19 +0800

    Add files via upload

---
+++ iOS_自动布局之masonry的高级使用/iOS_自动布局之masonry的高级使用/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  iOS_自动布局之masonry的高级使用
+//
+//  Created by 高宇 on 16/7/5.
+//  Copyright © 2016年 高宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    UILabel *displayNum;
+    NSString *symbol;
+}
+
+@property (strong, nonatomic) NSMutableString *num1;
+
+@property (strong, nonatomic) NSMutableString *num2;
+
+@end
+
